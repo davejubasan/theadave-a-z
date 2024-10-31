9c2ab862c9a8096bd2e3c8a447105d643a839618
Dave Jubasan <dave.jubasan@hitachi-dps.com>
2024-11-01 00:23:58 +0800

    add swiper card effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CardComponent } from './components/card/card.component';
 import { TitleComponent } from './components/title/title.component';
+import data from '../assets/data.json';
+import { ICard } from './models/ICard';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CardComponent, TitleComponent],
   templateUrl: './app.component.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppComponent {}
+export class AppComponent {
+  cardData: ICard[] = data;
+}
