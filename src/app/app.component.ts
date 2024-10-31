23ed0f5917ca37de47ac86809b5c271c053fcf24
Dave Jubasan <dave.jubasan@hitachi-dps.com>
2024-10-31 17:17:25 +0800

    add title component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {CardComponent} from './components/card/card.component';
+import { CardComponent } from './components/card/card.component';
+import { TitleComponent } from './components/title/title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, TitleComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        "coiny": ["Coiny-Regular"],
-        "give-you-glory": ["GiveYouGlory-Regular"]
-      },
-      colors: {
-        "color-a": "#6447E3",
-        "color-b": "#D778CA",
-        "color-c": "#FF7327",
-        "dark": "#5F5F5F"
-      },
-      screens: {
-        mobile: '360px',
-        tablet: '640px',
-        md: '768px',
-        lg: '976px',
-        laptop: '1024px',
-        desktop: '1280px',
-        xl: '1440px',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/**/*.{html,ts}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				"coiny": ["Coiny-Regular"],
+				"give-you-glory": ["GiveYouGlory-Regular"],
+				"charmonman": ["Charmonman-Regular"]
+			},
+			colors: {
+				"color-a": "#6447E3",
+				"color-b": "#FF7327",
+				"color-c": "#6447E3",
+				"dark": "#5F5F5F"
+			},
+			screens: {
+				mobile: '360px',
+				tablet: '640px',
+				md: '768px',
+				lg: '976px',
+				laptop: '1024px',
+				desktop: '1280px',
+				xl: '1440px',
+			},
+		},
+	},
+	plugins: [],
 }
